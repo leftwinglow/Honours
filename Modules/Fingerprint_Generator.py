be0590135c8742e7b2c9292721b76ff72b551164
Polarace <luke-a-thompson@outlook.com>
2023-11-12 17:45:25 +1100

    SMILES > tensor or np.ndarray

--- Modules/Fingerprint_Generator.py
+++ Modules/Fingerprint_Generator.py
@@ -1,7 +1,10 @@
 import pandas as pd
+import numpy as np
+import torch
 from molfeat import trans
+from typing import TypeVar
 
-def generate_fp_column(dataframe, dataframe_smiles_col, fp_type: str) -> pd.DataFrame:
+def generate_fp_column(dataframe: pd.DataFrame, dataframe_smiles_col, fp_type: str) -> pd.DataFrame:
     """Adds a column of SMILES strings with Molfeat fingerprints
 
     Args:
@@ -15,3 +18,34 @@ def generate_fp_column(dataframe, dataframe_smiles_col, fp_type: str) -> pd.Data
     fp_transformer = trans.MoleculeTransformer(featurizer=f'{fp_type}')
     dataframe[f"{fp_type}"] = fp_transformer.transform(dataframe_smiles_col.values)
     return dataframe
+
+class Smiles_To_Fingerprint():
+    T = TypeVar("T")
+    
+    def __init__(self, smiles: str, fp_type: str) -> None:
+        fp_transformer = trans.MoleculeTransformer(featurizer=fp_type)
+        self.fingerprint = fp_transformer.transform(smiles)
+    
+    # def astype(self, d_type: str):
+    #     match d_type:
+    #         case  'list':
+    #             return self.fingerprint
+    #         case 'numpy_array':
+    #             self.fingerprint = np.ndarray(self.fingerprint)
+    #         case 'torch_tensor':
+    #             self.fingerprint = torch.Tensor(self.fingerprint)
+    #         case _:
+    #             raise TypeError(f"Unsupported type: '{type}'")
+    #     return self.fingerprint
+    
+    def astype(self, return_type: type[T]) -> T:
+        match return_type:
+            case list() | tuple():
+                pass
+            case np.ndarray:
+                self.fingerprint = np.array(self.fingerprint)
+            case torch.Tensor:
+                self.fingerprint = torch.tensor(self.fingerprint)
+            case _:
+                raise TypeError(f"Unsupported type: '{type}'")
+        return self.fingerprint
